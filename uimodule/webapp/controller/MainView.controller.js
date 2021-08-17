7a8cca1c3c627cdbea2637fbf38aaab5697bfd3b
neo <pangsezi@gmail.com>
2021-08-17 14:28:51 +0900

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/sfc/helloWorld/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.sfc.helloWorld.controller.MainView", {});
+});
